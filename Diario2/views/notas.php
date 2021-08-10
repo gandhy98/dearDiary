b8ce4998b7f579e3121acc9deb9b305275a94578
Gandhy <gandhyquispe@gmail.com>
2021-08-09 20:42:02 -0500

    haciendo html

--- Diario2/views/notas.php
+++ Diario2/views/notas.php
@@ -1,34 +1,76 @@
-<!doctype html>
-<html lang="es">
-    <head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
         
         <!-- header links -->
         <?php
-            include_once ("./views/modules/headerPrin.html");
+            include_once ("./views/modules/header.html");
         ?>
         
-        <title>NOTAS | DIARY</title>
+        <title>PRINCIPAL | DIARY</title>
 </head>
 <body>
-    <main>  
-        <div>
+
+    <!-- Footer pages -->
+    <?php
+        include_once ("./views/pages/nav_principal.html");
+    ?>
+    <!-- insert notes to dear diary -->
+
+    <section class="container text-white">
+        <div class="row g-3">
             <form action="../notas.php" method="post">
-                <div>
-                    <input type="text" name="titulo" placeholder="ingresa titulo">
-                    <input type="date" name="fecha">
+                <div class="mb-3 col-6">
+                    <label for="exampleFormControlInput1" class="form-label">Titulo</label>
+                    <input type="text" 
+                        class="form-control" 
+                        id="exampleFormControlInput1" 
+                        name="titulo"
+                        placeholder="INGREAR TITULO" 
+                    >
+
                 </div>
-                <div>
-                    <textarea name="descripcion" rows="5"></textarea>
+                <div class="mb-3 col-3">
+                    <label for="exampleFormControlInput1" class="form-label">FECHA</label>
+                    <input type="date" 
+                        class="form-control" 
+                        name="fecha"
+                        id="exampleFormControlInput1" 
+                    >
                 </div>
+                <div class="mb-3 col-md-12">
+                    <label for="exampleFormControlTextarea1" 
+                        class="form-label" 
+                        name="descripcion"
+                    >
+                        DETALLE DE LA NOTA
+                    </label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+                </div>
+
                 <div>
                     <input type="submit" value="GUARDAR">
                     <a href="../principal.php">cancelar</a>
                 </div>
-            </form>
+            </form> 
         </div>
-    </main>
-     <!-- Footer links -->
-   <?php
+
+    </section>
+
+    
+    <!-- Footer pages -->
+    <?php
+        //include_once ("./views/pages/footer.html");
+    ?>
+
+
+    <!-- Footer links -->
+    <?php
         include_once ("./views/modules/footer.html");
-    ?> 
+    ?>
+
+
+
+
 </body>
+</html>
--- Diario2/views/principal.php
+++ Diario2/views/principal.php
@@ -4,7 +4,7 @@
         
         <!-- header links -->
         <?php
-            include_once ("./views/modules/headerPrin.html");
+            include_once ("./views/modules/header.html");
         ?>
         
         <title>PRINCIPAL | DIARY</title>
@@ -16,16 +16,96 @@
         include_once ("./views/pages/nav_principal.html");
     ?>
     <!-- insert notes to dear diary -->
-    <main>
-        <div>
-            <input class="#" onClick="entrarNotas()" type="text" placeholder = "¿tienes algo que contarme?">
-        </div>
-    </main>
 
-   <!-- Footer links -->
-   <?php
+    <div class="container pt-3">
+
+        <!-- boton notas -->
+        <button type="button" class="bton2" data-bs-toggle="modal" 
+            data-bs-target="#agregarNota"    
+        >
+            ¿tienes algo que contarme?
+        </button>
+
+        <!-- Modal agregar nota -->
+        <?php
+            include("./views/components/nota-modal.html");
+        ?>
+
+        <!-- todas las notas publicas -->
+
+        <section class="pt-4">
+
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+        <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+        <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. lorem100</p>
+      </div>
+        <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+        <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+    </div>
+  </div>
+</div>
+
+
+        </section>
+
+
+
+
+    </div>
+
+
+    <!-- Footer pages -->
+    <?php
+        //include_once ("./views/pages/footer.html");
+    ?>
+
+
+    <!-- Footer links -->
+    <?php
         include_once ("./views/modules/footer.html");
-    ?> 
+    ?>
+
 
 
 
